d204c72d534d9794b8d72d53d7a70b094498ac8c
Laura Amaro <79660843+laumagg@users.noreply.github.com>
2024-06-28 17:02:09 +0200

    Created Main Scene and PT Controller

--- Assets/Scripts/PassthroughController.cs
+++ Assets/Scripts/PassthroughController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PassthroughController : MonoBehaviour
 {
     [SerializeField] private OVRPassthroughLayer underlayPt;
     [SerializeField] private float startOpacity = 0.5f;
+    [SerializeField] private float transitionSpeed = 0.1f;
+
+    //TODO trigger changing opacity from Pomodoro timer
 
     private void Start()
     {
@@ -13,6 +18,20 @@ public class PassthroughController : MonoBehaviour
 
         underlayPt.enabled = true;
         underlayPt.textureOpacity = startOpacity;
+    }
+
+    private IEnumerator ChangeOpacity(float endOpacity)
+    {
+        float startOpacity = underlayPt.textureOpacity;
+        bool incrementing = startOpacity < endOpacity;
+
+        while (incrementing ? underlayPt.textureOpacity < endOpacity : underlayPt.textureOpacity > endOpacity)
+        {
+            underlayPt.textureOpacity = Mathf.Lerp(startOpacity, endOpacity, transitionSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+        underlayPt.textureOpacity = endOpacity;
     }
 
 
