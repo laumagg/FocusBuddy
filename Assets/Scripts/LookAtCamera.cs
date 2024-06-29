f12366f8d42c1f7faac582680001dd3df6587092
Leonid Barsht <leonidb93@gmail.com>
2024-06-30 00:39:25 +0200

    expand lookAt for y

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -4,6 +4,7 @@ public class LookAtCamera : MonoBehaviour
 {
     [SerializeField] private bool onlyOnStart = false;
     [SerializeField] private bool reversed = false;
+    [SerializeField] private bool onlyY = false;
     private Transform target;
     private void Init()
     {
@@ -15,12 +16,30 @@ public class LookAtCamera : MonoBehaviour
         if (target == null)
             Init();
 
-        if (target != null)
+        if (target != null && !onlyY)
         {
             transform.LookAt(reversed ? 2 * transform.position - target.position : target.position);
 
             if (onlyOnStart)
                 enabled = false;
+        }
+
+        if (onlyY)
+            LookAtY();
+    }
+
+    private void LookAtY()
+    {
+        if (target != null)
+        {
+            Vector3 targetPosition = target.position;
+            targetPosition.y = transform.position.y; // Keep the target position at the same height as this object
+
+            Vector3 direction = targetPosition - transform.position;
+            Quaternion toRotation = Quaternion.LookRotation(direction);
+            Quaternion rotation = Quaternion.Euler(0, toRotation.eulerAngles.y, 0);
+
+            transform.rotation = rotation;
         }
     }
 }
